c90fc034e0c11020069c183180b84cc4090c237b
RC Chuah <44928288+rc-chuah@users.noreply.github.com>
2020-09-23 17:28:13 +0800

    Add files via upload

---
+++ update-apkmod.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo Clearing Terminal !!!!!!
+
+sleep 1
+
+clear
+
+sleep 1
+
+echo Updating Apkmod !!!!!!
+
+sleep 1
+
+apkmod -u
+
+sleep 1
+
+echo Apkmod Successfully Updated !!!!!!
